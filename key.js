eb51203b40bdb0168e18682b6342c5a3bfe16c19
KunnAOV <killervn08042006@gmail.com>
2024-08-27 20:42:35 +0700

    Add files via upload

---
+++ key.js
@@ -0,0 +1,7 @@
+const validKeys = {
+    mainKey: 'KunnAOV-2006xx', // Key chính đầu tiên
+    secondaryKey: 'jsvbdvkhxhb3917xx', // Key phụ đầu tiên
+    mainKey2: 'BANTOMOD-12355', // Key chính thứ hai
+    mainKey3: 'GHOTSGREAM-12355', // Key chính thứ ba
+    secondaryKey2: 'abcxyz-3917yy' // Key phụ thứ hai
+};
